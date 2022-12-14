cceb68c7d1125c205ef811e84e013f7889ce7f59
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-12-14 18:51:53 -0300

    fix: max date for birthday and cpf pattern

--- src/schemas/customer.schema.js
+++ src/schemas/customer.schema.js
@@ -3,6 +3,6 @@ import joi from 'joi'
 export const customerSchema = joi.object({
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(11).required(),
-    birthday: joi.date().required()
+    cpf: joi.string().length(11).pattern(/^[0-9]{11}/).required(),
+    birthday: joi.date().max("now").required()
 })
